c0e8ea07e5aa16b49f2ab394824271e4488af836
Abhishek Jagushte <48720143+abhishekjagushte@users.noreply.github.com>
2024-02-08 22:16:56 +0530

    Event queue (#3)
    * eventQueue header file added
    
    * updated eventQueue header file
    
    ---------
    
    Co-authored-by: Mentalistv <varngupta2001@gmail.com>

---
+++ utilityFunc/eventQueue/eventQueue.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <ctime>
+
+using namespace std;
+
+class Event{
+    enum event_type{
+        GENERATE_TRANSACTION,
+        RECEIVE_TRANSACTION,
+        MINE_BLOCK,
+        RECEIVE_BLOCK
+    };
+
+    public:
+
+    time_t arrival_time;
+
+    void process_next_event();
+};
+
+typedef struct Event Event;
+
+class Compare {
+public:
+    bool operator()(Event a, Event b)
+    {
+        if (a.arrival_time < b.arrival_time) {
+            return true;
+        }
+        return false;
+    }
+};
+
+priority_queue <int, vector<int>, Compare> eventQueue;
