f74e4440d3f667b75baf4f764d45d19d97c58da1
Mentalistv <varngupta2001@gmail.com>
2024-02-10 23:04:01 +0530

    change node vector type

--- events/MineBlock.cpp
+++ events/MineBlock.cpp
@@ -4,18 +4,18 @@ void MineBlock::processEvent() {
     // Node *miner = &nodes[miner_id];
     int new_prev_block_id;
     
-    int curr_deepest_block_id = nodes[miner_id].deepest_block_id;
+    int curr_deepest_block_id = nodes[miner_id]->deepest_block_id;
 
     // checking if its the part of the longest chain
     if(curr_deepest_block_id != prev_block_id){
-        int curr_deepest_block_height = nodes[miner_id].blocks[curr_deepest_block_id].height;
+        int curr_deepest_block_height = nodes[miner_id]->blocks[curr_deepest_block_id].height;
 
         int temp = prev_block_id;
         int given_chain_height = 0;
 
-        while(nodes[miner_id].blocks[temp].id != -1){
+        while(nodes[miner_id]->blocks[temp].id != -1){
             given_chain_height++;
-            temp = nodes[miner_id].blocks[temp].id;
+            temp = nodes[miner_id]->blocks[temp].id;
         }
 
         if(given_chain_height < curr_deepest_block_height)  return;
@@ -27,7 +27,7 @@ void MineBlock::processEvent() {
 
     // MOD
 
-    auto itr = nodes[miner_id].txn_pool.begin();
+    auto itr = nodes[miner_id]->txn_pool.begin();
     int count = 1023;
     vector <Transaction> txn_to_be_included;
 
@@ -37,7 +37,7 @@ void MineBlock::processEvent() {
     }
 
     for(auto x: txn_to_be_included){
-        nodes[miner_id].txn_pool.erase(x.TXNID);
+        nodes[miner_id]->txn_pool.erase(x.TXNID);
     }
 
     Block* new_block = new Block("id", miner_id, new_prev_block_id, time, txn_to_be_included);
--- headers/MineBlock.h
+++ headers/MineBlock.h
@@ -4,7 +4,7 @@ class MineBlock : public Event {
     int miner_id;
     int prev_block_id;
 
-    MineBlock(vector<Node> &nodes, double time, event_type type, int miner_id) : Event(nodes, time, type), miner_id(miner_id), prev_block_id(prev_block_id){}
+    MineBlock(vector<Node*> &nodes, double time, event_type type, int miner_id) : Event(nodes, time, type), miner_id(miner_id), prev_block_id(prev_block_id){}
 
     void processEvent() override;
 
